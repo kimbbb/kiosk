7b4cfb7382156cec433f858985197b75810dc982
김민지 <wehouse201@naver.com>
2022-11-22 20:13:32 +0900

    login pug

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if(req.session.userId != undefined)
+    res.render('index', { title: 'Express', userId:req.session.userId });
+  else
+    res.render('index', { title: 'Express' });
+});
+
+router.get("/menu",(req,res)=>{
+  res.render("menu")
+})
+
+router.get("/shop",(req,res)=>{
+  res.render("shop")
+})
+
+module.exports = router;
