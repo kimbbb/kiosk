bd5405a2019e05684dcb592330ea41a010e97b56
김민지 <wehouse201@naver.com>
2022-11-25 11:06:48 +0900

    uplode

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
+const db = require('../model/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,5 +22,39 @@ router.get("/shop",(req,res)=>{
 router.get("/plus",(req,res)=>{
   res.render("plus")
 })
+
+const storage  = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
+
+
+router.post('/apiUpload', upload.single('file'), (req, res)=>{
+  const {title, message} = req.body;
+  let originalFileName='';
+  let fileUrl='';
+  if(req.file){
+    originalFileName=req.file.originalname;
+    fileUrl='/uploads/'+req.file.filename;
+  };
+  res.json({title:title, message:message, originalFileName:originalFileName, fileUrl:fileUrl});
+});
+
+router.post('/formUpload', upload.single('file'), (req, res)=>{
+  const {title, message} = req.body;
+  let originalFileName='';
+  let fileUrl='';
+  if(req.file){
+    originalFileName=req.file.originalname;
+    fileUrl='/uploads/'+req.file.filename;
+  };
+
+  res.render('form', {result:{title:title, message:message, originalFileName:originalFileName, fileUrl:fileUrl}});
+});
 
 module.exports = router;
